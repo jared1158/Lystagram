633e6dd8ce12350797b4643747a645c78a18d687
jared1158 <jaredlns@gmail.com>
2018-07-10 16:41:21 -0700

    progress update

--- app/src/main/java/com/jaredapps/lystagram/HomeActivity.java
+++ app/src/main/java/com/jaredapps/lystagram/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.jaredapps.lystagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.jaredapps.lystagram.model.Post;
 import com.parse.FindCallback;
@@ -21,6 +25,19 @@ public class HomeActivity extends AppCompatActivity {
         postsQuery
                 .getTop()
                 .withUser();
+
+        final int REQUEST_IMAGE_CAPTURE = 1;
+
+        final Button pic = findViewById(R.id.btnPic);
+        pic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
 
         postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
