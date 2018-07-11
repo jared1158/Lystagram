3a8ba4ec6a5eac22ce1b9ab9b6655706e04aaca2
jared1158 <jaredlns@gmail.com>
2018-07-10 22:38:47 -0700

    progress update

--- app/src/main/java/com/jaredapps/lystagram/HomeActivity.java
+++ app/src/main/java/com/jaredapps/lystagram/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.jaredapps.lystagram;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +15,10 @@ import com.jaredapps.lystagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -26,18 +33,47 @@ public class HomeActivity extends AppCompatActivity {
                 .getTop()
                 .withUser();
 
+
+
+
+
+
+
+
+
+
+
+
         final int REQUEST_IMAGE_CAPTURE = 1;
 
         final Button pic = findViewById(R.id.btnPic);
-        pic.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+        pic.setOnClickListener(new View.OnClickListener()
+
+    {
+        public void onClick (View v){
+        // Code here executes on main thread after user presses button
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                    //...
             }
-        });
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(HomeActivity.this,
+                        "com.codepath.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+        }
+     }});
+
 
         postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
@@ -56,5 +92,27 @@ public class HomeActivity extends AppCompatActivity {
                 }
 
         });
+    }
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
